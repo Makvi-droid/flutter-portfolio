bf6e508ec06aa688c4473595b9ba74cf19cef443
Makvi-droid <pasbenauro05@gmail.com>
2025-12-15 15:17:21 +0800

    finished simple portfolio project made in flutter

--- lib/skills_section.dart
+++ lib/skills_section.dart
@@ -48,6 +48,86 @@ class SkillsSection extends StatelessWidget {
                   style: TextStyle(color: Colors.white, fontSize: 16),
                 ),
               ),
+
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 12,
+                ),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF0F172A),
+                  borderRadius: BorderRadius.circular(24),
+                  border: Border.all(
+                    color: const Color.fromARGB(255, 239, 251, 20),
+                    width: 1.5,
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color.fromARGB(
+                        255,
+                        239,
+                        251,
+                        20,
+                      ).withOpacity(0.25),
+                      blurRadius: 12,
+                    ),
+                  ],
+                ),
+                child: const Text(
+                  'JavaScript',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ),
+
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 12,
+                ),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF0F172A),
+                  borderRadius: BorderRadius.circular(24),
+                  border: Border.all(
+                    color: const Color.fromARGB(255, 251, 89, 20),
+                    width: 1.5,
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color.fromARGB(255, 251, 89, 20),
+                      blurRadius: 12,
+                    ),
+                  ],
+                ),
+                child: const Text(
+                  'HTML',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ),
+
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 12,
+                ),
+                decoration: BoxDecoration(
+                  color: const Color(0xFF0F172A),
+                  borderRadius: BorderRadius.circular(24),
+                  border: Border.all(
+                    color: const Color.fromARGB(255, 6, 201, 255),
+                    width: 1.5,
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color.fromARGB(255, 6, 201, 255),
+                      blurRadius: 12,
+                    ),
+                  ],
+                ),
+                child: const Text(
+                  'CSS',
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ),
             ],
           ),
         ],
